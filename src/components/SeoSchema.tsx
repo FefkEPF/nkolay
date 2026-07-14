e7a6493a22fa54e84e5aa640cd817fef093e75a5
FefkEPF <fefkepf@users.noreply.github.com>
2026-07-14 19:51:12 +0300

    auto: sync changes from 2026-07-14T16:51:12.165Z

---
+++ src/components/SeoSchema.tsx
@@ -0,0 +1,16 @@
+import { organizationSchema, localBusinessSchema } from "../lib/schema";
+
+export default function SeoSchema() {
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
+      />
+    </>
+  );
+}
